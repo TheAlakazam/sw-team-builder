d64fd66243f75432804ffd99a98f55943ee3f49a
Piyush Jaipuriyar <piyushjaipuriyar@gmail.com>
2021-02-18 17:35:31 +0530

    feat: Added InstructionsPage scaffold

---
+++ src/components/InstructionsPage/InstructionsPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Card, CardBody, CardText, CardTitle} from 'reactstrap';
+
+const InstructionsPage = () => {
+  return (
+    <Card>
+      <CardBody>
+        <CardTitle tag="h3">Instructions</CardTitle>
+        <CardText>
+        </CardText>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default InstructionsPage;
