b95be042c9340c1bc185321ee957605da4d54565
Piyush Jaipuriyar <piyushjaipuriyar@gmail.com>
2021-02-18 17:33:59 +0530

    chore: Linter fixes

--- src/App.js
+++ src/App.js
@@ -1,57 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import {Container, Row} from 'reactstrap';
+import Header from './components/Header';
+import InstructionsPage from './components/InstructionsPage';
+import BuildingsPage from './components/BuildingsPage';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Container fluid className="p-0">
+      <Router>
+        <Header/>
+        <Row>
+          <Switch>
+            <Route path="/instructions">
+              <InstructionsPage/>
+            </Route>
+            <Route path="/buildings">
+              <BuildingsPage/>
+            </Route>
+          </Switch>
+        </Row>
+      </Router>
+    </Container>
   );
 }
 
